6ba9871aeab32ad21513bba94271d7330173def6
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-10-28 13:54:32 +0530

    download single file endpoint

--- routes/file.js
+++ routes/file.js
@@ -82,4 +82,23 @@ router.get('/getAllFiles', async (req, res) => {
 
 });
 
+// *********************************************** Get Single Uploaded File *********************************************
+router.get('/download/:id', async (req, res) => {
+  try {
+
+    const file = await File.findById(req.params.id);
+    res.set({
+      'Content-Type': file.file_mimetype
+    });
+
+    res.sendFile(path.join(__dirname, '..', file.file_path));
+
+  } catch (error) {
+
+    res.status(400).send('Error while downloading file. Try again later.');
+
+  }
+
+});
+
 module.exports = router;
